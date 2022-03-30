9942d1b958e42f9b677a72d4bb0419ccf9f8328e
Cong Liu <stormliucong@gmail.com>
2022-03-30 17:26:04 -0400

    dmd update

--- analysis/DMD_example.R
+++ analysis/DMD_example.R
@@ -12,9 +12,8 @@ url = "https://rare.cohd.io/api/association/obsExpRatio"
 res = GET(url, query = list(concept_id_1 = concept_id, dataset_id = i, domain_id = "phenotypes", top_n = 9999))
 topHpo = fromJSON(rawToChar(res$content))$results[,c(1,3,6)]
 dim(topHpo)
-topHpo
-
-topHpo %>% write.csv(file = "./DMD_top_HPO_oddsRatio.csv")
+topHpoDMD = as.data.table(topHpo)
+topHpoDMD[, disease:= q]
 
 q = "juvenile dermatomyositis"
 url = "https://rare.cohd.io/api/vocabulary/findConceptByName"
@@ -24,5 +23,7 @@ url = "https://rare.cohd.io/api/association/obsExpRatio"
 res = GET(url, query = list(concept_id_1 = concept_id, dataset_id = i, domain_id = "phenotypes", top_n = 9999))
 topHpo = fromJSON(rawToChar(res$content))$results[,c(1,3,6)]
 dim(topHpo)
+topHpoJDM = as.data.table(topHpo)
+topHpoJDM[, disease:= q]
 
-topHpo = topHpo1 topHpo2
+rbind(topHpoDMD,topHpoJDM) %>% fwrite(file="topHPO_DMD_JDM.csv")
