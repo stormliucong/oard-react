d65d4fa774f93c08834eaac83ec8a852f3aa0aec
Cong Liu <stormliucong@gmail.com>
2022-03-30 17:24:57 -0400

    dmd example add jmd

--- analysis/DMD_example.R
+++ analysis/DMD_example.R
@@ -1,10 +1,28 @@
 library(httr)
 library(jsonlite)
+library(data.table)
 
+q = "Duchenne muscular dystrophy"
 url = "https://rare.cohd.io/api/vocabulary/findConceptByName"
-res = GET(url, query = list(q = "Duchenne muscular dystrophy", domain_id = "diseases"))
+res = GET(url, query = list(q = q, domain_id = "diseases"))
 concept_id = fromJSON(rawToChar(res$content))$results$concept_id
 
+i =2
 url = "https://rare.cohd.io/api/association/obsExpRatio"
-res = GET(url, query = list(concept_id_1 = concept_id, dataset_id = 2, domain_id = "phenotypes", top_n = 200))
+res = GET(url, query = list(concept_id_1 = concept_id, dataset_id = i, domain_id = "phenotypes", top_n = 9999))
 topHpo = fromJSON(rawToChar(res$content))$results[,c(1,3,6)]
+dim(topHpo)
+topHpo
+
+topHpo %>% write.csv(file = "./DMD_top_HPO_oddsRatio.csv")
+
+q = "juvenile dermatomyositis"
+url = "https://rare.cohd.io/api/vocabulary/findConceptByName"
+res = GET(url, query = list(q = q, domain_id = "diseases"))
+concept_id = fromJSON(rawToChar(res$content))$results$concept_id
+url = "https://rare.cohd.io/api/association/obsExpRatio"
+res = GET(url, query = list(concept_id_1 = concept_id, dataset_id = i, domain_id = "phenotypes", top_n = 9999))
+topHpo = fromJSON(rawToChar(res$content))$results[,c(1,3,6)]
+dim(topHpo)
+
+topHpo = topHpo1 topHpo2
